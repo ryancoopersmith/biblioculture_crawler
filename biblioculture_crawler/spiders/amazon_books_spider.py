3416e9049748790ac970b94e29975610aebf3c70
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-12 15:08:29 -0400

    Rename amazon spider

--- biblioculture_crawler/spiders/amazon_books_spider.py
+++ biblioculture_crawler/spiders/amazon_books_spider.py
@@ -1,8 +1,8 @@
 import scrapy
 from scrapy import Selector
 
-class BooksSpider(scrapy.Spider):
-    name = "books"
+class AmazonBooksSpider(scrapy.Spider):
+    name = "amazon_books"
     start_urls = [
         'https://www.amazon.com/b/ref=s9_acss_bw_en_BGG15eve_d_1_1_w?_encoding=UTF8&node=1&pd_rd_r=XH38QTWP922WGKVA3KP0&pd_rd_w=FM9M4&pd_rd_wg=poHR3&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-top-3&pf_rd_r=XH38QTWP922WGKVA3KP0&pf_rd_r=XH38QTWP922WGKVA3KP0&pf_rd_t=101&pf_rd_p=e8ce74da-9c3d-45b7-a3fd-ea13b4732f00&pf_rd_p=e8ce74da-9c3d-45b7-a3fd-ea13b4732f00&pf_rd_i=283155',
     ]
