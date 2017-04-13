5b1648e0456cff9060b40934f962a873b12fc801
Evianne Keeney <evianneelise@gmail.com>
2017-04-13 13:55:13 -0400

    adds model and piplines for spider config

--- biblioculture_crawler/pipelines.py
+++ biblioculture_crawler/pipelines.py
@@ -4,8 +4,41 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from sqlalchemy.orm import sessionmaker
+from models import Deals, db_connect, create_deals_table
 
 class BibliocultureCrawlerPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class AmazonBooksPipeline(object):
+    """AmazonBooks pipeline for storing scraped items in the database"""
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker.
+        Creates deals table.
+        """
+        engine = db_connect()
+        create_deals_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    def process_item(self, item, spider):
+        """Save deals in the database.
+
+        This method is called for every item pipeline component.
+
+        """
+        session = self.Session()
+        deal = Deals(**item)
+
+        try:
+            session.add(deal)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
         return item
