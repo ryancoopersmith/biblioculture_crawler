f89fd8ce5d38f79b4f8c51bb316816bcb6c053c2
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-13 15:35:14 -0400

    Update crawler

--- biblioculture_crawler/spiders/amazon_books_spider.py
+++ biblioculture_crawler/spiders/amazon_books_spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
+from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
 
 from scrapy.loader import XPathItemLoader
@@ -7,28 +8,41 @@ from scrapy.loader.processor import Join, MapCompose
 
 import AmazonBooks
 
-def product_info(response, value):
-    return response.xpath('//th[text()="' + value + '"]/following-sibling::td/text()').extract_first()
+# def product_info(response, value):
+#     return response.xpath('//th[text()="' + value + '"]/following-sibling::td/text()').extract_first()
 
 class AmazonBooksSpider(Spider):
     name = 'amazon_books'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com']
 
+    books_list_xpath = '//h3/a/@href'
+    item_fields = {
+        'title': './/h1/text()',
+        'price': './/*[@class="price_color"]/text()'
+    }
+
     def parse(self, response):
-        books = response.xpath('//h3/a/@href').extract()
-        for book in books:
-            absolute_url = response.urljoin(book)
-            yield Request(absolute_url, callback=self.parse_book)
+        selector = HtmlXPathSelector(response)
+
+        for book in selector.select(self.books_list_xpath):
+            loader = XPathItemLoader(AmazonBooks(), selector=book)
+
+            # define processors
+            loader.default_input_processor = MapCompose(unicode.strip)
+            loader.default_output_processor = Join()
+
+            # iterate over fields and add xpaths to the loader
+            for field, xpath in self.item_fields.iteritems():
+                loader.add_xpath(field, xpath)
+            yield loader.load_item()
+
+        loader = XPathItemLoader(AmazonBooks(), selector=book)
 
         # process next page
-        next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
-        absolute_next_page_url = response.urljoin(next_page_url)
-        yield Request(absolute_next_page_url)
-
-    def parse_book(self, response):
-        title = response.css('h1::text').extract_first()
-        price = response.xpath('//*[@class="price_color"]/text()').extract_first()}
+        # next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
+        # absolute_next_page_url = response.urljoin(next_page_url)
+        # yield Request(absolute_next_page_url)
 
         # image_url = response.xpath('//img/@src').extract_first()
         # image_url = image_url.replace('../..', 'http://books.toscrape.com/')
@@ -48,6 +62,7 @@ class AmazonBooksSpider(Spider):
         # availability = product_info(response, 'Availability')
         # number_of_reviews = product_info(response, 'Number of reviews')
 
-        yield {
-            'title': title,
-            'price': price}
+        # yield {
+        #     'title': title,
+        #     'price': price
+        #     }
